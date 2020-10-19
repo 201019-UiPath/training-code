1f2c62673f327ea971ab7fd2a36e801e17825366
Pushpinder <pushpinder.kaur@revature.com>
2020-10-20 02:08:35 +0530

    first program

---
+++ 1-CSharp/helloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿//1.0 predefined namespace
+using System;
+
+//1.1 custom defined namespace
+namespace helloWorld
+{
+    class Program
+    {
+        // execution starts here
+                        //Commandline arguments
+                        // vvv
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World! "+DateTime.Now);
+            string firstname, lastname;
+            Console.WriteLine("Please enter your first name");
+            firstname = Console.ReadLine();// takes user's input in string form
+            Console.WriteLine("Please enter your last name"); 
+            lastname = Console.ReadLine();
+            //Console.WriteLine("Welcome "+firstname +" "+lastname);
+            Console.WriteLine($"welcome {firstname}-{lastname}");
+        }
+    }
+}
