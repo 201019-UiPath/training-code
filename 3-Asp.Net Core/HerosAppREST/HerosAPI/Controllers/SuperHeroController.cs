873c1a3f1b188cc53894fc7592660cc7d6142be3
mariellenolasco <mariellenolasco@gmail.com>
2020-11-06 12:24:21 -0500

    HerosREST

---
+++ 3-Asp.Net Core/HerosAppREST/HerosAPI/Controllers/SuperHeroController.cs
@@ -0,0 +1,63 @@
+﻿using HerosDB.Models;
+using HerosLib;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HerosAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class SuperHeroController : ControllerBase
+    {
+        private readonly IHeroService _heroService;
+
+        public SuperHeroController(IHeroService heroService)
+        {
+            _heroService = heroService;
+        }
+
+        [HttpGet("get")]
+        [Produces("application/json")]
+        public IActionResult GetAllHeroes()
+        {
+            try
+            {
+                return Ok(_heroService.GetAllHeroes());
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("get/{name}")]
+        [Produces("application/json")]
+        public IActionResult GetHeroByName(string name)
+        {
+            try
+            {
+                return Ok(_heroService.GetHeroByName(name));
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("add")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public IActionResult AddHero(SuperHero newSuperHero)
+        {
+            try
+            {
+                _heroService.AddHero(newSuperHero);
+                return CreatedAtAction("AddHero", newSuperHero);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ 3-Asp.Net Core/HerosAppREST/HerosLib/IVillainService.cs
@@ -0,0 +1,12 @@
+﻿using HerosDB.Models;
+using System.Collections.Generic;
+
+namespace HerosLib
+{
+    public interface IVillainService
+    {
+        void AddVillain(SuperVillain newVillain);
+        List<SuperVillain> GetAllVillains();
+        SuperVillain GetVillainByName();
+    }
+}
