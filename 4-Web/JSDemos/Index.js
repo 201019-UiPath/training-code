b9a250a1d000211e07461d23678492dbb86299ab
mariellenolasco <mariellenolasco@gmail.com>
2020-11-10 17:47:35 -0500

    JSDemos

---
+++ 4-Web/JSDemos/Index.js
@@ -0,0 +1,36 @@
+function BasicFunction()
+{
+    alert("Hello World of JS!");
+}
+
+function ParentFunction(callBack)
+{
+    console.log("In parent function");
+    let name = prompt('Please enter your  name');
+    callBack(name);
+    console.log("Back in parent function");
+}
+
+function CallBack(name)
+{
+    console.log("Hello " + name);
+    console.log("calling back");
+}
+//This IIFE uses the outer function's count parameter
+let Outer = (() => 
+{
+    let count = 0;
+    return function inner()
+    {
+        return count += 1;
+    };
+})();
+let addMore = (() => {
+    let count = 0;
+    return () => {
+        count += 1;
+        return count;
+    };
+});
+let add = addMore();
+let addAgain = addMore();
